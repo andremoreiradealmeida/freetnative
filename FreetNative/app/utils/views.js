50d506d51ef755611ac2410d12e64a73da580b6e
Andre Almeida <andremoreiradealmeida@gmail.com>
2015-09-17 11:36:06 +0000

    Changes made by Andre Almeida (andremoreiradealmeida@gmail.com).

---
+++ FreetNative/app/utils/views.js
@@ -0,0 +1,11 @@
+var Views;
+
+(function (Views) {
+    Views.login = "components/login/login";
+    Views.cadastro = "components/cadastro/cadastro";
+    Views.ticketspage = "components/tickets-page/tickets-page";
+    Views.ticketsdetailpage = "components/tickets-detail-page/tickets-detail-page";
+    Views.cidadepage = "components/cidade-page/cidade";
+    Views.empresaspage = "components/empresas-page/empresas-page";
+    Views.ticketsempresa = "components/tickets-empresa/tickets-empresa";
+})(Views = exports.Views || (exports.Views = {}));
